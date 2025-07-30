dfc9a31f6eba7da013bafeadb670ad1545c9d6a4
BotModeB <163748886+BotModeB@users.noreply.github.com>
2025-07-30 17:50:34 +0300

    update

---
+++ src/main/java/com/TextHub/TextHub/TextHubApplication.java
@@ -0,0 +1,13 @@
+package com.TextHub.TextHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TextHub/TextHub/TextHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TextHub.TextHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
